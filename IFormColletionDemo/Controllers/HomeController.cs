03f678ea3d3418222a5182291b763f5195da91a0
kenan <kenan@LAPTOP-5KTTCAE1>
2021-06-01 09:49:17 +0200

    Add project files.

---
+++ IFormColletionDemo/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IFormColletionDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection collection)
+        {
+            ViewBag.FirstName = (collection["txtFirstName"]).ToString();
+            ViewBag.LastName = (collection["txtLastName"]).ToString();
+            ViewBag.Age = (collection["txtAge"]).ToString();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+    }
+}
